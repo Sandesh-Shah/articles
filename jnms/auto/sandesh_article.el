70aa4ff6ff5678881fc3987c8313e0535e71b3e8
sandeshthakuri111@gmail.com <sandeshthakuri111@gmail.com>
2024-06-13 11:49:10 +0545

    to jnms email

--- jnms/auto/sandesh_article.el
+++ jnms/auto/sandesh_article.el
@@ -7,13 +7,13 @@
                      '(("article" "a4paper" "twoside" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("caption2" "normal") ("inputenc" "utf8") ("babel" "english") ("subfigure" "normal")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
